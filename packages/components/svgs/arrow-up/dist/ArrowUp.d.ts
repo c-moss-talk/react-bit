1fd86b5baf742e8125ae1ab0aa75464a4dee37e7
Cameron Moss <cameron.moss@talkspace.com>
2020-05-04 10:14:07 -0400

    remove staged bit component and bump version

--- packages/components/svgs/arrow-up/dist/ArrowUp.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-declare const ArrowUp: ({ width, height, fill, ...otherProps }: {
-    [x: string]: any;
-    width?: number;
-    height?: number;
-    fill?: string;
-}) => JSX.Element;
-export default ArrowUp;
