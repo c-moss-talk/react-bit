959ddda9cd723dbf6e5ffeb2a41bcfdf1a53f6d7
Cameron Moss <cameron.moss@talkspace.com>
2020-05-04 12:37:34 -0400

    change color

---
+++ packages/components/svgs/arrow-up/dist/ArrowUp.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+declare const ArrowUp: ({ width, height, fill, ...otherProps }: {
+    [x: string]: any;
+    width?: number;
+    height?: number;
+    fill?: string;
+}) => JSX.Element;
+export default ArrowUp;
